21c2b4876d906852ed6238abdcfd92b5447dad8a
pablogerheim <pablogerheim@gmail.com>
2022-08-20 22:25:01 -0300

    swagger

--- back/src/doc.js
+++ back/src/doc.js
@@ -1,80 +1,338 @@
 export const swaggerDocument = {
-    swagger: '2.0',
-    info: {
-        description: 'My Bank API description',
-        version: '1.0.0',
-        title: 'My Bank API description',
+    "openapi": "3.0.3",
+    "info": {
+        "title": "Core Lab",
+        "version": "1.0.11"
     },
-    host: 'localhost:3000',
-    tags: [
+    "servers": [
         {
-            name: 'account',
-            description: 'Account management',
-        },
+            "url": "https://localhost:3002"
+        }
     ],
-    paths: {
-        '/account': {
-            get: {
-                tags: ['account'],
-                summary: 'Get existing accounts',
-                description: 'Get existing account description',
-                produces: ['application/json'],
-                responses: {
-                    200: {
-                        description: 'successful operation',
-                        schema: {
-                            type: 'array',
-                            items: {
-                                $ref: '#/definitions/Account',
-                            },
-                        },
-                    },
-                    400: {
-                        description: 'Error occurred',
-                    },
-                },
-            },
-            post: {
-                tags: ['account'],
-                summary: 'Create a new account',
-                description:
-                    'Create a new account with the received parameters',
-                consumes: ['application/json'],
-                parameters: [
-                    {
-                        in: 'body',
-                        name: 'body',
-                        description: 'Account object',
-                        required: true,
-                        schema: {
-                            $ref: '#/definitions/Account',
-                        },
-                    },
+    "tags": [
+        {
+            "name": "/",
+            "description": "Everything about your Vehicles",
+            "externalDocs": {
+                "description": "Find out more",
+                "url": "http://swagger.io"
+            }
+        }
+    ],
+    "paths": {
+        "/": {
+            "get": {
+                "tags": [
+                    "/"
                 ],
-                responses: {
-                    200: {
-                        description: 'Account created',
+                "summary": "Update an existing Vehicle",
+                "responses": {
+                    "200": {
+                        "description": "successful operation",
+                        "content": {
+                            "application/json": {
+                                "schema": {
+                                    "type": "array",
+                                    "items": {
+                                        "$ref": "#/components/schemas/VehicleU"
+                                    }
+                                }
+                            }
+                        }
                     },
-                    400: {
-                        description: 'Error occurred',
+                    "400": {
+                        "description": "Invalid tag value"
+                    }
+                }
+            },
+            "patch": {
+                "tags": [
+                    "/"
+                ],
+                "summary": "Togle vehicle",
+                "description": "make an existing vehicle favorite",
+                "operationId": "favorite vehicle",
+                "requestBody": {
+                    "content": {
+                        "application/json": {
+                            "schema": {
+                                "$ref": "#/components/schemas/Id"
+                            }
+                        }
+                    }
+                },
+                "responses": {
+                    "200": {
+                        "description": "successful operation",
+                        "content": {
+                            "application/json": {
+                                "schema": {
+                                    "type": "array",
+                                    "items": {
+                                        "$ref": "#/components/schemas/VehicleU"
+                                    }
+                                }
+                            }
+                        }
                     },
-                },
+                    "400": {
+                        "description": "Invalid tag value"
+                    }
+                }
             },
-        },
-    },
-    definitions: {
-        Account: {
-            type: 'object',
-            properties: {
-                name: {
-                    type: 'string',
-                    example: 'Guilherme Assis',
+            "post": {
+                "tags": [
+                    "/"
+                ],
+                "summary": "Add a new vehicle to the store",
+                "description": "Add a new vehicle to the store",
+                "operationId": "addVehicle",
+                "requestBody": {
+                    "description": "Create a new vehicle in the store",
+                    "content": {
+                        "application/json": {
+                            "schema": {
+                                "$ref": "#/components/schemas/VehicleC"
+                            }
+                        }
+                    }
                 },
-                balance: {
-                    type: 'number',
-                    example: 742.34,
-                },
+                "responses": {
+                    "200": {
+                        "description": "successful operation",
+                        "content": {
+                            "application/json": {
+                                "schema": {
+                                    "type": "array",
+                                    "items": {
+                                        "$ref": "#/components/schemas/VehicleU"
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    "400": {
+                        "description": "Invalid tag value"
+                    }
+                }
             },
+            "put": {
+                "tags": [
+                    "/"
+                ],
+                "summary": "Finds All products",
+                "description": "Update an existing Vehicle by Id",
+                "operationId": "updateVehicle",
+                "requestBody": {
+                    "description": "Update an existent vehicle in the store",
+                    "content": {
+                        "application/json": {
+                            "schema": {
+                                "$ref": "#/components/schemas/VehicleU"
+                            }
+                        }
+                    }
+                },
+                "responses": {
+                    "200": {
+                        "description": "successful operation",
+                        "content": {
+                            "application/json": {
+                                "schema": {
+                                    "type": "array",
+                                    "items": {
+                                        "$ref": "#/components/schemas/VehicleU"
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    "400": {
+                        "description": "Invalid tag value"
+                    }
+                }
+            }
         },
+        "/id": {
+            "get": {
+                "tags": [
+                    "/"
+                ],
+                "summary": "Finds Vehicle By Id",
+                "operationId": "Id",
+                "parameters": [
+                    {
+                        "name": "id",
+                        "in": "query",
+                        "description": "Tags to filter by",
+                        "required": true,
+                        "schema": {
+                            "type": "array",
+                            "items": {
+                                "type": "string"
+                            }
+                        }
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "successful operation",
+                        "content": {
+                            "application/json": {
+                                "schema": {
+                                    "type": "array",
+                                    "items": {
+                                        "$ref": "#/components/schemas/VehicleU"
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    "400": {
+                        "description": "Invalid tag value"
+                    }
+                }
+            },
+            "delete": {
+                "tags": [
+                    "/"
+                ],
+                "summary": "Deletes a vehicle",
+                "description": "delete a vehicle",
+                "operationId": "deleteVehicle",
+                "parameters": [
+                    {
+                        "name": "id",
+                        "in": "query",
+                        "description": "Vehicle id to delete",
+                        "required": true,
+                        "schema": {
+                            "type": "string"
+                        }
+                    }
+                ],
+                "responses": {
+                    "400": {
+                        "description": "Invalid pet value"
+                    }
+                }
+            }
+        }
     },
-};
+    "components": {
+        "schemas": {
+            "Id": {
+                "type": "object",
+                "properties": {
+                    "id": {
+                        "type": "string",
+                        "example": "10"
+                    }
+                }
+            },
+            "VehicleU": {
+                "type": "object",
+                "properties": {
+                    "id": {
+                        "type": "string",
+                        "format": "int64",
+                        "example": "10"
+                    },
+                    "name": {
+                        "type": "string",
+                        "example": "carro 1"
+                    },
+                    "description": {
+                        "type": "string",
+                        "example": "Primeiro carro"
+                    },
+                    "isFavorite": {
+                        "type": "boolean",
+                        "example": true
+                    },
+                    "year": {
+                        "type": "string",
+                        "example": "2018"
+                    },
+                    "color": {
+                        "type": "string",
+                        "example": "vermelho"
+                    },
+                    "price": {
+                        "type": "string",
+                        "example": "60500"
+                    },
+                    "createdAt": {
+                        "type": "string",
+                        "example": "2022-07-11T23:14:36.131Z"
+                    }
+                }
+            },
+            "VehicleC": {
+                "type": "object",
+                "properties": {
+                    "name": {
+                        "type": "string",
+                        "example": "carro 1"
+                    },
+                    "description": {
+                        "type": "string",
+                        "example": "Primeiro carro"
+                    },
+                    "isFavorite": {
+                        "type": "boolean",
+                        "example": true
+                    },
+                    "year": {
+                        "type": "string",
+                        "example": "2018"
+                    },
+                    "color": {
+                        "type": "string",
+                        "example": "vermelho"
+                    },
+                    "price": {
+                        "type": "string",
+                        "example": "60500"
+                    },
+                    "createdAt": {
+                        "type": "string",
+                        "example": "2022-07-11T23:14:36.131Z"
+                    }
+                }
+            }
+        },
+        "requestBodies": {
+            "VehicleU": {
+                "description": "Vehicle object that needs to be added to the store",
+                "content": {
+                    "application/json": {
+                        "schema": {
+                            "$ref": "#/components/schemas/VehicleU"
+                        }
+                    }
+                }
+            },
+            "VehicleC": {
+                "description": "Vehicle object that needs to be added to the store",
+                "content": {
+                    "application/json": {
+                        "schema": {
+                            "$ref": "#/components/schemas/VehicleC"
+                        }
+                    }
+                }
+            },
+            "Id": {
+                "description": "Id",
+                "content": {
+                    "application/json": {
+                        "schema": {
+                            "$ref": "#/components/schemas/Id"
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- back/src/routes/vehicles.routes.js
+++ back/src/routes/vehicles.routes.js
@@ -4,10 +4,10 @@ import vehiclesController from '../controllers/vehicles.controller.js';
 const router = express.Router();
 
 router.get('/', vehiclesController.getVehicles);
-router.get('/:id', vehiclesController.getVehicles);
 router.patch('/', vehiclesController.patchVehicles);
-router.delete('/:id', vehiclesController.deleteVehicle);
 router.post('/', vehiclesController.createVehicle);
 router.put('/', vehiclesController.updateVehicle);
+router.get('/:id', vehiclesController.getVehicles);
+router.delete('/:id', vehiclesController.deleteVehicle);
 
 export default router;
